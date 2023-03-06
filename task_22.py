1dba79313f3cd2cbb1824ef5f7b7525fe84f80a7
Konstantin <ksemenec@mail.ru>
2023-03-06 11:04:51 +0500

    задача 21

---
+++ task_22.py
@@ -0,0 +1,5 @@
+# Задача №22
+
+list_1 = set(list(map(int, input("введите числа через пробел ").split( ))))
+list_2 = set(list(map(int, input("введите числа через пробел ").split( ))))
+print(list_1.intersection(list_2))
